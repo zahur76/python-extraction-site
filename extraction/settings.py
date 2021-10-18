2ae84cfa430a8db8ba30bb9a9054e3f4dacbccdf
zahur <zahurmeerun@hotmail.com>
2021-10-18 17:41:20 +0400

    add home app and main page

--- extraction/settings.py
+++ extraction/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -67,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
