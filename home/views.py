60f362945a3bb5b649a9aa51c43a4b11be0f4888
zahur <zahurmeerun@hotmail.com>
2021-10-20 14:21:08 +0400

    update code to allow for change in class names by using RE

--- home/views.py
+++ home/views.py
@@ -5,10 +5,11 @@ import time
 import math
 from .models import Products
 import json
+import re
+
 
 def extractor(request):
-    """ A view to return the home page with site request"""
-    
+    """ A view to return the home page with site request"""    
 
     products = Products.objects.all()
     if products:
@@ -16,79 +17,64 @@ def extractor(request):
     
     url = "https://www.manomano.fr/scie-a-main-et-lames-de-scie-493" 
 
-    # Obtain items per page
-    response = requests.get(url)        
-    soup_one = BeautifulSoup(response.content, "html.parser")
-    results = soup_one.find_all('div', class_='title_a64399e6') 
-    items_per_page = 0          
-    for result in results:
-        items_per_page += 1
-    
     # Obtain number of pages
     response = requests.get(url)        
-    soup_two = BeautifulSoup(response.content, "html.parser")
-    result_one = soup_two.find('div', {'class':'Pagination_label__2nq-e'}).text.split()[-1]
-    
-    pages = math.ceil(int(result_one)/items_per_page)  
+    soup_one= BeautifulSoup(response.content, "html.parser")
+    result_one = soup_one.find('div', {'class':'Pagination_label__2nq-e'}).text.split()[-1]
+    result_two = soup_one.find('div', {'class':'Pagination_label__2nq-e'}).text.split()[0]
+    print(result_one)
+    print(result_two)
+    pages = math.ceil(int(result_one)/int(result_two))  
     count = 0
     product = {}
     product_dict = {}
-    # for page in range(0, pages+1):
-    for page in range(0, 3):
+    # for page in range(0, pages+1):    
+    for page in range(1, pages+1):        
+        url_two = "https://www.manomano.fr/scie-a-main-et-lames-de-scie-493"
+        print(url_two)        
         try:
-            response = requests.get(url, {"page": page})                
+            response = requests.get(url_two, {'page':page})                                               
         except requests.exceptions.RequestException as e:
             print(e)             
-        soup = BeautifulSoup(response.content, "html.parser")        
-        container = soup.find_all('a', class_='root_a64399e6')
+        soup = BeautifulSoup(response.content, "html.parser")             
         # items per page
-        results = soup.find_all('div', class_='title_a64399e6') 
-        items_per_page_two = 0          
-        for result in results:
-            items_per_page_two += 1 
-
+        if page == pages:
+            items_per_page_two = int(result_one) % int(result_two)        
+        else:
+            items_per_page_two = int(result_two)
+         
         print('this is page' + ' ' + str(page))
         print('items per page is' + ' ' + str(items_per_page_two))
-        
-        for x in range(0,items_per_page_two):        
+        container = soup.find_all('a', attrs={'class':re.compile('root_'),
+            'href': re.compile('/p/')})      
+           
+        for x in range(0,items_per_page_two):            
+            data =  container[x]                                       
             count += 1
-            product_name = container[x].find('div', class_='title_a64399e6')        
-            product_image_url = container[x].find('img')
-            product_rating = container[x].find('span', class_='stars_bbd09dd1')
-            product_price_main = container[x].find('span', class_='integer_f2d8fa2c')
-            product_price_decimal = container[x].find('span', class_='decimal_f2d8fa2c')
-
-            product_price = product_price_main.text + '.' + product_price_decimal.text
-            product_url = 'https://www.manomano.fr' + container[x].get('href')
-                    
-            if product_rating:
-                product_rating = product_rating.get('aria-label')
-                product_rating = float(product_rating.split('/')[0])
-            else:
+            product_url = 'https://www.manomano.fr' + data.get('href')                 
+            product_name = data.find('img')['alt'].split('of')[-1].replace('"', '')                                  
+            product_image_url = data.find('img').get('data-src')
+            try:
+                product_rating = data.find('span', attrs={'class':re.compile('stars_')}).get('aria-label')              
+            except:
                 product_rating = None
-            
-            # product[count] = {'product_name' : product_name.text, 
-            #     'product_price': product_price,
-            #     'product_url': product_url,
-            #     'product_image_url': product_image_url.get('data-src'),
-            #     'product_rating': product_rating,
-            #     }
+
+            if product_rating:                
+                product_rating =  float(product_rating.split('/')[0])
+            else:
+                product_rating = None                    
+                       
+            product_price_main = data.find('span', attrs={'class':re.compile('integer_')})
+            product_price_decimal = data.find('span', attrs={'class':re.compile('decimal_')})
+            product_price = product_price_main.text + '.' + product_price_decimal.text          
             product_dict = {'id':count,
-                'product_name' : product_name.text, 
+                'product_name' : product_name, 
                 'product_price': product_price,
                 'product_url': product_url,
-                'product_image_url': product_image_url.get('data-src'),
+                'product_image_url': product_image_url,
                 'product_rating': product_rating,
-                }
+                }            
             
-            # model_product = Products.objects.create(
-            #     product_name=product_name.text, 
-            #     product_price=float(product_price),
-            #     product_url=product_url,
-            #     product_image_url=product_image_url.get('data-src'),
-            #     product_rating=product_rating,                
-            #     )
-            # model_product.save()
             product_json_dict = json.dumps(product_dict)
             model_product = Products.objects.create(
                 id = count,
@@ -115,10 +101,10 @@ def home(request):
 
 def product_details(request, product_id):
     """ A view to return the product details"""
-    products = get_object_or_404(Products, id=product_id)
-    
+    print(product_id)
+    query = get_object_or_404(Products, id=product_id)
+    product = json.loads(query.product_json)
     context = {
-        'products': products,
+        'product': product,
         }
-
-    return render(request, 'home/index.html', context)
+    return render(request, 'home/product_details.html', context)
