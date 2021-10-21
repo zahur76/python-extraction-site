8c8e5e744d38c174aa7ea2c27b9030334b60fa04
zahur <zahurmeerun@hotmail.com>
2021-10-21 07:29:11 +0400

    add selenium for link finder

--- home/views.py
+++ home/views.py
@@ -5,7 +5,11 @@ import math
 from .models import Products
 import json
 import re
-from django.db.models import Q
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
 
 
 def extractor(request):
@@ -18,25 +22,21 @@ def extractor(request):
     url = "https://www.manomano.fr/scie-a-main-et-lames-de-scie-493" 
 
     # Obtain number of pages
-    response = requests.get(url)        
-    soup_one= BeautifulSoup(response.content, "html.parser")
+    response_one = requests.get(url)        
+    soup_one= BeautifulSoup(response_one.content, "html.parser")
     result_one = soup_one.find('div', {'class':'Pagination_label__2nq-e'}).text.split()[-1]
-    result_two = soup_one.find('div', {'class':'Pagination_label__2nq-e'}).text.split()[0]
-    print(result_one)
-    print(result_two)
+    result_two = soup_one.find('div', {'class':'Pagination_label__2nq-e'}).text.split()[0]    
     pages = math.ceil(int(result_one)/int(result_two))  
-    count = 0
-    product = {}
+    count = 0    
     product_dict = {}
-    # for page in range(0, pages+1):    
-    for page in range(1, pages+1):        
-        url_two = "https://www.manomano.fr/scie-a-main-et-lames-de-scie-493"
-        print(url_two)        
+    # for page in range(1, pages+1):       
+    for page in range(1, 2):        
+        url_two = "https://www.manomano.fr/scie-a-main-et-lames-de-scie-493"               
         try:
-            response = requests.get(url_two, {'page':page})                                               
+            response_two = requests.get(url_two, {'page':page})                                               
         except requests.exceptions.RequestException as e:
             print(e)             
-        soup = BeautifulSoup(response.content, "html.parser")             
+        soup_two = BeautifulSoup(response_two.content, "html.parser")             
         # items per page
         if page == pages:
             items_per_page_two = int(result_one) % int(result_two)        
@@ -45,10 +45,11 @@ def extractor(request):
          
         print('this is page' + ' ' + str(page))
         print('items per page is' + ' ' + str(items_per_page_two))
-        container = soup.find_all('a', attrs={'class':re.compile('root_'),
-            'href': re.compile('/p/')})      
-           
-        for x in range(0,items_per_page_two):            
+        container = soup_two.find_all('a', attrs={'class':re.compile('root_'),
+            'href': re.compile('/p/')})
+
+        # for x in range(0,items_per_page_two): 
+        for x in range(0,2):            
             data =  container[x]                                       
             count += 1
             product_url = 'https://www.manomano.fr' + data.get('href')                 
@@ -62,8 +63,8 @@ def extractor(request):
             if product_rating:                
                 product_rating =  float(product_rating.split('/')[0])
             else:
-                product_rating = None                    
-                       
+                product_rating = None
+
             product_price_main = data.find('span', attrs={'class':re.compile('integer_')})
             product_price_decimal = data.find('span', attrs={'class':re.compile('decimal_')})
             product_price = product_price_main.text + '.' + product_price_decimal.text          
@@ -80,8 +81,37 @@ def extractor(request):
                 id = count,
                 product_json=product_json_dict,                           
                 )
-            model_product.save()        
-        
+            model_product.save()
+
+            # Product detail page
+            options = webdriver.ChromeOptions()            
+            driver = webdriver.Chrome("C:/Users/chromedriver.exe", chrome_options=options)                         
+            try:
+                driver.get(product_url)               
+            except requests.exceptions.RequestException as e:
+                print(e)
+
+            driver.execute_script('return document.body.innerHTML')            
+                       
+            driver.find_element_by_class_name('CybotCookiebotDialogBodyButton').click()
+            
+            
+            main_seller= driver.find_element_by_css_selector("a[href*='/marchand-']")    
+            main_seller_name = main_seller.text
+            print(main_seller_name)            
+            
+            # open-up modal using selenium          
+
+            button = driver.find_element_by_xpath("//*[text()='autres marchands']")
+            driver.execute_script("arguments[0].click();", button)
+            WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, "a[href*='/p/']")))
+            
+            offer_links = driver.find_elements_by_css_selector("a[href*='/p/']") 
+            for a in offer_links:
+                print(a.get_attribute("href"))           
+            
+            driver.quit()
+            
     return redirect(reverse('home'))
 
 
